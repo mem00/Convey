0cc08811fdcc715d7a1c9bb47ebbab73acd8500c
Michael McGuire <michael3mcguire@gmail.com>
2019-07-15 10:08:59 -0400

    changed frontend route

--- client/src/Constants/index.js
+++ client/src/Constants/index.js
@@ -1,6 +1,6 @@
 // https://medium.com/@dakota.lillie/using-action-cable-with-react-c37df065f296
-export const API_ROOT = ''
-export const API_WS_ROOT = ''
+export const API_ROOT = 'https://convey-messenger.herokuapp.com/'
+export const API_WS_ROOT = 'ws://convey-messenger.herokuapp.com/'
 export const HEADERS = {
     'Content-Type': 'application/json',
      Accept : 'application/json'
